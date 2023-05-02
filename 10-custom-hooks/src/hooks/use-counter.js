62c1f5691bfb13958280d55167b8d83c79e0cf4f
Michael Hofweller <bigviking@Michaels-MacBook-Pro.local>
2023-05-02 11:01:33 -0700

    cleanup

---
+++ 10-custom-hooks/src/hooks/use-counter.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+const useCounter = (increase) => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (increase) {
+        setCounter((prevCounter) => prevCounter + 1);
+      } else {
+        setCounter((prevCounter) => prevCounter - 1);
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [increase]);
+
+  return counter;
+};
+export default useCounter;
